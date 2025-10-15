8dc589ca63151670450f3a786fbac9f50d21e041
sequoia <kluger_und_starker@icloud.com>
2025-10-15 18:59:08 +0300

    new_project

--- start.py
+++ start.py
@@ -1,18 +1,21 @@
-import pygame as pg
+import pygame as p
 
-pg.init()
+p.init()
 
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 550
 
+PERS_WDT = 300
+PERS_HGT = 375
+
 FPS = 60
 
-font = pg.font.Font(None, 40)
+font = p.font.Font(None, 40)
 
 
 def load_image(file, width, height):
-    image = pg.image.load(file).convert_alpha()
-    image = pg.transform.scale(image, (width, height))
+    image = p.image.load(file).convert_alpha()
+    image = p.transform.scale(image, (width, height))
     return image
 
 
@@ -20,27 +23,51 @@ def text_render(text):
     return font.render(str(text), True, "black")
 
 
-class Player(pg.sprite.Sprite):
+class Player(p.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        self.image = 0 #refactor
+        self.load_animations()
+
+        self.image = self.idle_anim_right[0]
+        self.current_image = 0
+        self.animations = self.idle_anim_right
+
+        self.time = p.time.get_ticks()
+        self.interval = 150
 
         self.rect = self.image.get_rect()
         self.rect.center = (100, SCREEN_HEIGHT // 2)
 
+    def load_animations(self):
+        self.idle_anim_right = []
+        for i in range(1, 4):
+            self.idle_anim_right.append(load_image(f"images/fire_wizard/idle{i}.png", PERS_WDT, PERS_HGT))
+
+        self.idle_anim_left = []
+        for image in self.idle_anim_right:
+            self.idle_anim_left.append(p.transform.flip(image, True, False)) #true - вертикаль, false - горизонталь
+
+    def update(self):
+        if p.time.get_ticks() - self.time >= self.interval:
+            self.current_image += 1
+            if self.current_image >= len(self.animations):
+                self.current_image = 0
+                self.time = p.time.get_ticks()
 
 class Game:
     def __init__(self):
 
         # Создание окна
-        self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        pg.display.set_caption("Битва магов")
-        window_icon = pg.Surface().icon()
+        self.screen = p.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        p.display.set_caption("Битва магов")
+        p.display.set_caption("Wizard Battle")
+        p.display.set_icon(load_image("images/icon.png", 10, 10))
 
         self.background = load_image("images/background.png", SCREEN_WIDTH, SCREEN_HEIGHT)
 
-        self.clock = pg.time.Clock()
+        self.player = Player()
+        self.clock = p.time.Clock()
         self.run()
 
     def run(self):
@@ -51,18 +78,18 @@ class Game:
             self.clock.tick(FPS)
 
     def event(self):
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
+        for event in p.event.get():
+            if event.type == p.QUIT:
                 quit()
 
     def update(self):
-        pass
+        self.player.update()
 
     def draw(self):
         # Отрисовка интерфейса
         self.screen.blit(self.background, (0, 0))
-
-        pg.display.flip()
+        self.screen.blit(self.player.animations[self.player.current_image], self.player.rect)
+        p.display.flip()
 
 
 if __name__ == "__main__":
